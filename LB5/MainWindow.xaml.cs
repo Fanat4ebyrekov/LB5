b76711afd2eb733d11a177d6c2ca4f2eab28ca0d
Fanat4ebyrekov <81490021+Fanat4ebyrekov@users.noreply.github.com>
2021-10-18 21:46:53 +0300

    Добавьте файлы проекта.

---
+++ LB5/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LB5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItemCloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuItemGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Vb");
+        }
+
+        private void menuItemChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItemColorBlack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Black;
+        }
+
+        private void menuItemColorGreen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Green;
+        }
+
+        private void menuItemColorOrange_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Orange;
+        }
+
+        private void menuItemColorBlack_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBlockProperty.Text = "Это кнопка изменения цвета на черный";
+        }
+
+        private void menuItemColorGreen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBlockProperty.Text = "Это кнопка изменения цвета на зеленый ";
+        }
+
+        private void menuItemColorOrange_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBlockProperty.Text = "Это кнопка изменения цвета на оранжевый";
+        }
+
+        private void menuItemGetInfo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBlockProperty.Text = "Это кнопка покажет информацию о программисте";
+        }
+
+        private void menuItemCloseWindow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBlockProperty.Text = "Это кнопка закроет окно";
+        }
+
+        private void btnToNext_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow second = new SecondWindow();
+            second.ShowDialog();
+        }
+
+        private void toolBarItemColorChange_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ LB5/SecondWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LB5
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox1.Style = (Style)Resources["SubTextBox"];
+            TextBox2.Style = (Style)Resources["SubTextBox"];
+            TextBox3.Style = (Style)Resources["SubTextBox"];
+            (sender as TextBox).Style = (Style)Resources["MainTextBox"];
+        }
+
+        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox1.Style = (Style)Resources["SubTextBox"];
+            TextBox2.Style = (Style)Resources["SubTextBox"];
+            TextBox4.Style = (Style)Resources["SubTextBox"];
+            (sender as TextBox).Style = (Style)Resources["MainTextBox"];
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox1.Style = (Style)Resources["SubTextBox"];
+            TextBox3.Style = (Style)Resources["SubTextBox"];
+            TextBox4.Style = (Style)Resources["SubTextBox"];
+            (sender as TextBox).Style = (Style)Resources["MainTextBox"];
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox2.Style = (Style)Resources["SubTextBox"];
+            TextBox3.Style = (Style)Resources["SubTextBox"];
+            TextBox4.Style = (Style)Resources["SubTextBox"];
+            (sender as TextBox).Style = (Style)Resources["MainTextBox"];
+        }
+    }
+}
